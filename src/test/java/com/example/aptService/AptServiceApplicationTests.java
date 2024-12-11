fc4567e9f1d801c7fc90edd800e30398f63bac84
ddubab <ddu9872@naver.com>
2024-12-11 17:38:57 +0900

    초기 프로젝트 생성

---
+++ src/test/java/com/example/aptService/AptServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.aptService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AptServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
